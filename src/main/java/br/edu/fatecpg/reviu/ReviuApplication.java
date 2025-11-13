c9ed1d75f3b269359e5b82b9f19227e36f9bc792
thialms <thiiago.allmeida44@gmail.com>
2025-11-13 15:51:22 -0300

    Project created, initial settings established

---
+++ src/main/java/br/edu/fatecpg/reviu/ReviuApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.reviu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/fatecpg/reviu/ReviuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.reviu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
